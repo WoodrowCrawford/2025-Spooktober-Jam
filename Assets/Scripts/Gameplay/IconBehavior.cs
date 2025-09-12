3fde8ba2eb4ed594c6d0b42fc4e2118f4b2b38aa
Woodrow Crawford <woodrowcrawford03@gmail.com>
2025-09-11 22:26:06 -0400

    Started on desktop ui
    -Added a click and drag feature for the icons

---
+++ Assets/Scripts/Gameplay/IconBehavior.cs
@@ -0,0 +1,101 @@
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class IconBehavior : MonoBehaviour, IPointerUpHandler, IPointerDownHandler, IPointerEnterHandler, IPointerExitHandler
+{
+
+   
+
+    [Header("Icon Settings")]
+    [SerializeField] private Color _hoverColor = Color.lightGray;
+    [SerializeField] private Color _clickedColor = Color.gray;
+    [SerializeField] private float _timeHeldDown = 0f;
+
+    [Header("Bool settings")]
+    [SerializeField] private bool _isClicked = false;
+
+    public UnityEvent onIconClicked;
+
+
+
+
+    //what happens when the icon is clicked
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("Pointer down on icon: " + gameObject.name);
+        _isClicked = true;
+
+        GetComponent<Image>().color = _clickedColor;
+    
+    }
+
+
+    //what happens when the icon is released
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+       
+        Debug.Log("Pointer released on icon: " + gameObject.name);
+        _isClicked = false;
+
+        GetComponent<Image>().color = Color.white;
+
+   
+    }
+
+
+    //what happens when the pointer enters the icon area
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("Pointer entered icon: " + gameObject.name);
+        GetComponent<Image>().color = _hoverColor;
+
+    }
+
+
+    //what happens when the pointer exits the icon area
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("Pointer exited icon: " + gameObject.name);
+        GetComponent<Image>().color = Color.white;
+    }
+
+
+
+
+  
+  
+    private void Update()
+    {
+
+        //use a timer based feature to determine if the icon is draggable
+        if (_isClicked)
+        {
+            //start a timer
+            _timeHeldDown += Time.deltaTime;
+
+            //after a while, make the icon draggable
+            if (_timeHeldDown >= 0.2f)
+            {
+                //move the icon to the mouse position
+                this.transform.position = Mouse.current.position.ReadValue();
+            }
+
+        }
+        else
+        {
+            this.transform.position = this.transform.position;
+            _timeHeldDown = 0f;
+        }
+
+      
+        
+
+    }
+
+
+    
+}
