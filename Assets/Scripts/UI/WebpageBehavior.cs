879880e4ff2e44500e89517cfded45616765a488
Woodrow Crawford <woodrowcrawford03@gmail.com>
2025-09-16 22:43:17 -0400

    Still working on webpages; Imported ui images
    - Imported ui images for reddit like website

--- Assets/Scripts/UI/WebpageBehavior.cs
+++ Assets/Scripts/UI/WebpageBehavior.cs
@@ -1,16 +1,8 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
-public class WebpageBehavior : MonoBehaviour
+public class WebpageBehavior : MonoBehaviour, IPointerUpHandler, IPointerDownHandler
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
